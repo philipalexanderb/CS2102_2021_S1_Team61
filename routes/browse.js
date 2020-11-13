0b4b754297a88e27db367bf159789f9d3e4ce690
kelvinnharris <kelvinnharris@gmail.com>
2020-11-13 16:51:38 +0800

    fix get browse page

--- routes/browse.js
+++ routes/browse.js
@@ -11,63 +11,48 @@ const pool = new Pool({
 router.get("/", middleware(), function (req, res, next) {
   const username = req.session.passport.user;
 
-  var pets = [];
-  pool.query(sql_query.query.get_pets, [username], (err, data) => {
-    pets = data.rows;
-  });
+  // var pets = [];
+  // pool.query(sql_query.query.get_pets, [username], (err, data) => {
+  //   pets = data.rows;
+  //   console.log(data.rows);
+  // });
   
   var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
-
   pool.query(sql_query.query.browse, [], (err, data) => {
     var username = req.session.passport.user;
-    console.log(username);
+    // console.log(username);
 
     // to offset ISO string convention timezone
     for (var i = 0; i < data.rows.length; i++) {
       data.rows[i].s_date = (new Date(data.rows[i].s_date - tzoffset))
       data.rows[i].e_date = (new Date(data.rows[i].e_date - tzoffset))
-      console.log(data.rows[i].s_date);
+      // console.log(data.rows[i].s_date);
     }
-    console.log(data.rows);
+    // console.log(data.rows);
 
-    res.render("browse", { avails: data.rows, username: username, pets: pets });
+    // res.render("browse", { avails: data.rows, username: username, pets: pets });
+    res.render("browse", { avails: data.rows, username: username });
   });
 });
 
 router.get("/:id", function (req, res, next) {
-  // console.log(req);
-  // console.log(req.params.id);
   const username = req.params.id;
   const pousername = req.session.passport.user;
 
-  // console.log("########### POUNAME FOR BROWSE ###################");
-  // console.log(pousername);
-
-  // console.log("########### USERNAME FOR BROWSE ###################");
-  // console.log(username);
-
-  var bids = [];
-  pool.query(sql_query.query.get_bid, [username], (err, data) => {
-    bids = data.rows;
-  });
-
-  // console.log("###################### BIDS HERE #####################")
-  // console.log(bids);
-
-  setTimeout(() => {
+  // get bids for a caretaker
+  pool.query(sql_query.query.get_bid, [username], (err, bids_res) => {
+    // get other data for caretaker
     pool.query(sql_query.query.get_browsed_caretaker, [req.params.id], (err, data) => {
-      // console.log(data);
       res.render("browsed_caretaker", {
         username: data.rows[0].username,
         pousername: pousername,
         address: data.rows[0].address,
         first_name: data.rows[0].first_name,
         last_name: data.rows[0].last_name,
-        bids: bids
+        bids: bids_res.rows
        })
     });
-  }, 2000); 
-
+  });
 });
 
 module.exports = router;
